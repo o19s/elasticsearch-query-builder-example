6c718679af76d52c1cd5cf60aecbf88f0527f5d9
Doug Turnbull <softwaredoug@gmail.com>
2019-03-02 09:46:37 -0500

    Build process works, but compiler errors to figure out

---
+++ src/main/java/com/o19s/es/explore/ExplorerQueryBuilder.java
@@ -0,0 +1,137 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.o19s.es.explore;
+
+import com.o19s.es.ltr.utils.AbstractQueryBuilderUtils;
+import org.apache.lucene.search.Query;
+import org.elasticsearch.common.ParseField;
+import org.elasticsearch.common.ParsingException;
+import org.elasticsearch.common.io.stream.NamedWriteable;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ObjectParser;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.index.query.AbstractQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryShardContext;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ExplorerQueryBuilder extends AbstractQueryBuilder<ExplorerQueryBuilder> implements NamedWriteable {
+    public static final String NAME = "match_explorer";
+
+    private static final ParseField QUERY_NAME = new ParseField("query");
+    private static final ParseField TYPE_NAME = new ParseField("type");
+    private static final ObjectParser<ExplorerQueryBuilder, Void> PARSER;
+
+    static {
+        PARSER = new ObjectParser<>(NAME, ExplorerQueryBuilder::new);
+        PARSER.declareObject(
+                ExplorerQueryBuilder::query,
+                (parser, context) -> parseInnerQueryBuilder(parser),
+                QUERY_NAME
+        );
+        PARSER.declareString(ExplorerQueryBuilder::statsType, TYPE_NAME);
+        AbstractQueryBuilderUtils.declareStandardFields(PARSER);
+    }
+
+    private QueryBuilder query;
+    private String type;
+
+    public ExplorerQueryBuilder() {
+    }
+
+
+    public ExplorerQueryBuilder(StreamInput in) throws IOException {
+        super(in);
+        query = in.readNamedWriteable(QueryBuilder.class);
+        type = in.readString();
+    }
+
+    public static ExplorerQueryBuilder fromXContent(XContentParser parser) throws IOException {
+        final ExplorerQueryBuilder builder;
+
+        try {
+            builder = PARSER.parse(parser, null);
+        } catch (IllegalArgumentException iae) {
+            throw new ParsingException(parser.getTokenLocation(), iae.getMessage(), iae);
+        }
+
+        if (builder.query == null) {
+            throw new ParsingException(parser.getTokenLocation(), "Field [" + QUERY_NAME + "] is mandatory.");
+        }
+        if (builder.statsType() == null) {
+            throw new ParsingException(parser.getTokenLocation(), "Field [" + TYPE_NAME + "] is mandatory.");
+        }
+        return builder;
+    }
+
+    @Override
+    protected void doWriteTo(StreamOutput out) throws IOException {
+        out.writeNamedWriteable(query);
+        out.writeString(type);
+    }
+
+    @Override
+    protected void doXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject(NAME);
+        printBoostAndQueryName(builder);
+        builder.field(QUERY_NAME.getPreferredName(), query);
+        builder.field(TYPE_NAME.getPreferredName(), type);
+        builder.endObject();
+    }
+
+    @Override
+    protected Query doToQuery(QueryShardContext context) throws IOException {
+        return new ExplorerQuery(query.toQuery(context), type);
+    }
+
+    @Override
+    protected int doHashCode() {
+        return Objects.hash(query, type);
+    }
+
+    @Override
+    protected boolean doEquals(ExplorerQueryBuilder other) {
+        return Objects.equals(query, other.query)
+                && Objects.equals(type, other.type);
+    }
+
+    @Override
+    public String getWriteableName() {
+        return NAME;
+    }
+
+    public QueryBuilder query() {
+        return query;
+    }
+
+    public ExplorerQueryBuilder query(QueryBuilder query) {
+        this.query = query;
+        return this;
+    }
+
+    public String statsType() {
+        return type;
+    }
+
+    public ExplorerQueryBuilder statsType(String type) {
+        this.type = type;
+        return this;
+    }
+}
---
+++ src/main/java/com/o19s/es/explore/ExplorerScorer.java
@@ -0,0 +1,86 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.o19s.es.explore;
+
+import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.Scorer;
+import org.apache.lucene.search.Weight;
+
+import java.io.IOException;
+
+public class ExplorerScorer extends Scorer {
+    private final Scorer subScorer;
+    private final String type;
+
+    protected ExplorerScorer(Weight weight, String type, Scorer subScorer) {
+        super(weight);
+        this.type = type;
+        this.subScorer = subScorer;
+    }
+
+    @Override
+    public float score() throws IOException {
+        StatisticsHelper tf_stats = new StatisticsHelper();
+
+        // Grab freq from subscorer, or the children if available
+        if(subScorer.getChildren().size() > 0) {
+            for(ChildScorer child : subScorer.getChildren()) {
+                assert child.child instanceof PostingsExplorerQuery.PostingsExplorerScorer;
+                if(child.child.docID() == docID()) {
+                    tf_stats.add(child.child.score());
+                }
+            }
+        } else {
+            assert subScorer instanceof PostingsExplorerQuery.PostingsExplorerScorer;
+            assert subScorer.docID() == docID();
+            tf_stats.add(subScorer.score());
+        }
+
+        float retval;
+        switch(type) {
+            case("sum_raw_tf"):
+                retval = tf_stats.getSum();
+                break;
+            case("mean_raw_tf"):
+                retval = tf_stats.getMean();
+                break;
+            case("max_raw_tf"):
+                retval = tf_stats.getMax();
+                break;
+            case("min_raw_tf"):
+                retval = tf_stats.getMin();
+                break;
+            case("stddev_raw_tf"):
+                retval = tf_stats.getStdDev();
+                break;
+            default:
+                throw new RuntimeException("Invalid stat type specified.");
+        }
+
+        return retval;
+    }
+
+    @Override
+    public int docID() {
+        return subScorer.docID();
+    }
+
+
+    @Override
+    public DocIdSetIterator iterator() {
+        return subScorer.iterator();
+    }
+}
